1cd39aa9c6e6e1fa3c71219980e0ed89e50e8ec3
LoopViewOnly <85384343+LoopViewOnly@users.noreply.github.com>
2024-07-12 18:13:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const fakepic = "https://thispersondoesnotexist.com/";
+
+function realPic() {
+  const id = Math.floor((Math.random() * 6 + 1) * 10000);
+  return `https://whichfaceisreal.blob.core.windows.net/public/realimages/${id}.jpeg`;
+}
+function game() {
+  const imageCon = document.getElementById("images");
+  const resultCon = document.getElementById("results");
+  const streakCon = document.getElementById("streak");
+  let streak = 0;
+  function draw() {
+    imageCon.innerHTML = "";
+    resultCon.innerHTML = "";
+
+    const again = document.createElement("button");
+    again.textContent = "play again!";
+    again.onclick = draw;
+
+    const rand = Math.random() > 0.5;
+    const arr = [rand, !rand];
+    for (const isReal of arr) {
+      const img = document.createElement("img");
+      img.src = isReal ? realPic() : fakepic;
+      imageCon.appendChild(img);
+      img.onclick = function () {
+        resultCon.textContent = isReal
+          ? "you are Correct!"
+          : "you are Incorrect";
+        streak = isReal ? streak + 1 : 0;
+        streakCon.innerHTML = ("streak:", +streak);
+
+        resultCon.appendChild(again);
+      };
+    }
+  }
+  draw();
+}
+
+game();
